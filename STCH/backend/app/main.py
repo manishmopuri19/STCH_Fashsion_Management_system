342eb876e13996333a90a8abab1a9edf10d33bda
manishmopuri19 <manishsreeram2@gmail.com>
2026-06-03 15:29:09 +0530

    netlify issue solved

--- STCH/backend/app/main.py
+++ STCH/backend/app/main.py
@@ -89,11 +89,11 @@ app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 
 # CORS
+allowed_origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:5173").split(",")
 
 app.add_middleware(
     CORSMiddleware,
-
-    allow_origins="*",
+    allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
