fb5bf40bb66e6a9a277c12327c48d98968aca8cc
ManishMopuri23 <maneesh@stch.com>
2026-08-24 11:38:26 +0530

    added backend url path

--- STCH/frontend/src/api/axios.js
+++ STCH/frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-   // baseURL: 'https://stch.onrender.com',
-    baseURL:'http://127.0.0.1:5000'
+    baseURL: 'https://stch.onrender.com',
+   // baseURL:'http://127.0.0.1:5000'
 });
 
 API.interceptors.request.use((config) => {
